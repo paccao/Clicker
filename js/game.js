cc09c68d0ece97e630d0184f9ca4ddf2cbec805e
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2020-12-22 22:21:54 +0100

    pseudocode for the main game loop, afternoon div

--- js/game.js
+++ js/game.js
@@ -1,1 +1,8 @@
 import { show, hide, find } from "./util"
+
+/// game loop
+// Next day function (update span element in day-number)
+// Morning function
+// Afternoon function
+// Evening function
+// End game function (if you lose)
