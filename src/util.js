239a4eff5b22805c009de82ef7ae6371df03586c
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-01-23 15:23:33 +0100

    delete react stuff
    Co-authored-by: Samuel Plumppu <Greenheart@users.noreply.github.com>

--- src/util.js
+++
@@ -1,9 +0,0 @@
-/**
- * Return a random item from given array
- *
- * @param {any[]} array The array
- */
- export const getRandomItemFromArray = (array) => {
-    const index = Math.floor(Math.random() * array.length)
-    return array[index]
-}
